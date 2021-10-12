3f7f8f3066cc4ba87c3b2217a319598eaf893136
sdoperry <78126970+sdoperry@users.noreply.github.com>
2021-10-11 19:56:34 -0500

    Module 6 Final Project - Math Blog

---
+++ client-math-blog/README.md
@@ -0,0 +1,13 @@
+# Final Project for Module 6
+
+This is my final project for Module 6 of Kingsland University's Zero-to-Blockchain course.
+Author: Sharrod Perry
+
+## Project Description
+
+A simple blog, regarding Math topics, where blogs can be added, viewed, and deleted.
+
+### Using React
+
+This project was assigned to assess student's understanding of React JS.
+I used Components, state, and hooks such as useState and useEffect to build this app.
---
+++ client-math-blog/src/Home.js
@@ -0,0 +1,23 @@
+import BlogList from "./Bloglist";
+import useFetch from "./useFetch";
+
+const Home = () => {
+	const {
+		data: blogs,
+		isPending,
+		error,
+	} = useFetch("http://localhost:8000/blogs");
+
+	return (
+		<div className="home">
+			{error && <div>{error}</div>}
+			{isPending && <div>Loading...</div>}
+			{blogs && (
+				<BlogList blogs={blogs} title="THE MAJOR CATEGORIES OF MATH" />
+			)}
+			{/* <button onClick={() => setName("luigi")}>change name</button> */}
+		</div>
+	);
+};
+
+export default Home;
